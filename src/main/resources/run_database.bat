12c63af4de024ca83e669b0e2d29ce8a910d29e2
Diogo Mendonça <Diogo Mendona>
2016-11-12 15:29:01 -0200

    Ajustado .bat para funcionar corretamente no windows.

--- src/main/resources/run_database.bat
+++ src/main/resources/run_database.bat
@@ -1,1 +1,1 @@
-java -cp ..\..\..\target\webdep\WEB-INF\lib\hsqldb-2.3.1.jar org.hsqldb.server.Server --database.0 database/webdep --dbname.0 webdep
+java -cp %USERPROFILE%\.m2\repository\org\hsqldb\hsqldb\2.3.1\hsqldb-2.3.1.jar org.hsqldb.server.Server --database.0 database/webdep --dbname.0 webdep
