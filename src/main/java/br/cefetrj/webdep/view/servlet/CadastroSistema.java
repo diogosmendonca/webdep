76dec4991bbdc64f0641d8a4c45d49daa4204dfe
Luan <Luan@192.168.1.9>
2016-11-12 21:00:25 -0200

    Adição de Servlets de Cadastro, Gerenciamento de Sistema e Adição de Padrao URL

--- src/main/java/br/cefetrj/webdep/view/servlet/CadastroSistema.java
+++ src/main/java/br/cefetrj/webdep/view/servlet/CadastroSistema.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.cefetrj.webdep.view.controller;
+package br.cefetrj.webdep.view.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
