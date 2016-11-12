13386a775dca39ab03cc63881aa50671fff47a72
Luan <Luan@192.168.1.9>
2016-11-12 19:36:36 -0200

    Compilável do cadastro de Padrão de URL
    To do:
    
    - Descobrir quem é o usuário logado que está cadastrando o novo padrão
    
    - Descobrir usuário logado e definir quais URL serão permitadas serem
    filtradas pela expressão regular

--- src/main/java/br/cefetrj/webdep/view/servlet/NovoPadraoURL.java
+++ src/main/java/br/cefetrj/webdep/view/servlet/NovoPadraoURL.java
@@ -7,12 +7,25 @@ package br.cefetrj.webdep.view.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import br.cefetrj.webdep.model.dao.PersistenceManager;
+
+import org.hibernate.tuple.CreationTimestampGeneration;
+
 import br.cefetrj.webdep.model.entity.PadraoURL;
+import br.cefetrj.webdep.model.entity.RegistroLogAcesso;
+import br.cefetrj.webdep.model.entity.Sistema;
+import br.cefetrj.webdep.model.dao.GenericDAO;
+import br.cefetrj.webdep.model.dao.PersistenceManager;
 
 /**
  *
@@ -27,7 +40,12 @@ public class NovoPadraoURL extends HttpServlet {
         dispatcher.forward(request,response);
     }*/
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -54,9 +72,14 @@ public class NovoPadraoURL extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         PrintWriter pw = response.getWriter();
+        PersistenceManager pm = PersistenceManager.getInstance(); 
+        GenericDAO<RegistroLogAcesso> regLogAcessoDAO = pm.createGenericDAO(RegistroLogAcesso.class);
+        List<RegistroLogAcesso> registrosLogAcesso = regLogAcessoDAO.listAll();
+        //retrieving URL Pattern form fields
         String action = request.getParameter("action");
         String nome = request.getParameter("nome");
         String regex = request.getParameter("regex");
+        //validating form
         if ("salvarPadrao".equals(action)) {
             String erro_json = "{ \"Erro\": [";
             boolean erro = false;
@@ -74,12 +97,17 @@ public class NovoPadraoURL extends HttpServlet {
             erro_json = erro_json.replace(",]}","]}");
             if (erro) pw.write(erro_json);
             else {
-                /*Inserção no banco*/
-                //PadraoURLsDAO urlDAO = new PadraoURLsDAO();
+                //form validation succeded
+            	for (RegistroLogAcesso registroLogAcesso: registrosLogAcesso){
+            		
+            	}
                 PadraoURL padraoURL = new PadraoURL();
                 padraoURL.setNome(nome);
                 padraoURL.setExpressaoRegular(regex);
+                
+                //padraoURL.setUsuario(usuario); // usuario logado quem é?
                 //urlDAO.addPadrao(padraoURL);
+                
                 pw.write("success");
             }
         } else if ("buscaRegex".equals(action)) {
@@ -87,31 +115,27 @@ public class NovoPadraoURL extends HttpServlet {
             String json = "";
             response.setContentType("text/plain");
             try{
-                /*  Pattern p = Pattern.compile(regex);
-                    Aqui utiliza-se o DAO do outro grupo que está responsável por guardar as URLs no banco.
-                    A intenção é pegar todas as URLs do banco:
-                    
-                    URLsDAO urlDAO = new URLsDAO();
-                    List<String> urlsFiltradas = new ArrayList<>();
-                    List<String> urls = urlDAO.getAll();
-                    for (String url: urls){
-                        Matcher m = p.matcher(url);
-                        if (m.matches()){
-                            urlsFiltradas.add(url);
-                        }
-                    }
-                    //Transformar as URLs filtradas em JSON pra mandar de volta 
-                    //pra página
-                    json = "{\"url\": [";
-                    for (String url: urlsFiltradas){
-                        json += "\"";
-                        json += url; //talvez tenha que add um "/" no inicio
-                        json += "\", "
-                    }
-                    json += "]}";
-                    json = json.replace(",]}", "]}");
-                */
-                json = "{\"url\": [\"/index.php\", \"/home.php\", \"/cadastro.php\"]}";
+                Pattern p = Pattern.compile(regex);
+                    /*Aqui utiliza-se o DAO do outro grupo que está responsável por guardar as URLs no banco.
+                    A intenção é pegar todas as URLs do banco:*/
+                List<String> URLsRegex = new ArrayList<String>();
+                for (RegistroLogAcesso registroLogAcesso: registrosLogAcesso){
+                	//depois que descobrir quem é o usuário logado.. 
+                	//verificar permissão para ver registros de log de acesso
+                	String url = registroLogAcesso.getUrl();
+                	Matcher m = p.matcher(url);
+                	if (m.matches()) URLsRegex.add(url);
+            	}
+                //Transformar as URLs filtradas em JSON pra mandar de volta 
+                //pra página
+                json = "{\"url\": [";
+                for (String url: URLsRegex){
+                    json += "\"";
+                    json += url; //talvez tenha que add um "/" no inicio
+                    json += "\", ";
+                }
+                json += "]}";
+                json = json.replace(",]}", "]}");
                 pw.write(json);
             }catch(Exception e){
                 
