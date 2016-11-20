bc8f21b5284fb436aefd14712325d873ca03822b
Marcello <marcello.serqueira@gmail.com>
2016-11-20 18:38:44 -0200

    Validação dos campos da página HTTP report

--- src/main/java/br/cefetrj/webdep/view/command/ValidaHttpReportCommand.java
+++ src/main/java/br/cefetrj/webdep/view/command/ValidaHttpReportCommand.java
@@ -13,13 +13,38 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ValidaHttpReport")
 public class ValidaHttpReportCommand implements Command {
 	@Override
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ArrayList<String> version = new ArrayList<>();
-		ArrayList<String> errorCode = new ArrayList<>();
-		ArrayList<String> okCode = new ArrayList<>();
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		boolean versionValidate = true;
+		boolean errorValidate = true;
+		boolean okValidate = true;
+
+		if(request.getParameterValues("versionList") != null){
+			String versionList[] = request.getParameterValues("versionlist");
+		} else {
+			versionValidate = false;
+			request.setAttribute("versionValidate", versionValidate);
+		}
 		
-		System.out.println(request.getParameter("errorList"));
+		if(request.getParameterValues("errorList") != null){
+			String versionList[] = request.getParameterValues("errorList");
+		} else {
+			errorValidate = false;
+			request.setAttribute("erorValidate", errorValidate);
+		}
 		
+		if(request.getParameterValues("okList") != null){
+			String versionList[] = request.getParameterValues("okList");
+		} else {
+			okValidate = false;
+			request.setAttribute("okValidate", okValidate);
+		}
+		
+		if((versionValidate == false) || (errorValidate == false) || (okValidate = false)){
+			request.getRequestDispatcher("HTTPreport.jsp").forward(request, response);
+			return;
+		} else {
+			//Lógica para a busca dos filtros...
+		}
 		
 	}
 
