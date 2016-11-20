bd466f2b7e1f8645bfe7d063f063e4664333eee2
Lawrence Fernandes <lawrence.stfs@gmail.com>
2016-11-20 16:42:54 -0200

    Inserção da Command para logout

---
+++ src/main/java/br/cefetrj/webdep/view/command/DeslogaUsuarioCommand.java
@@ -0,0 +1,19 @@
+package br.cefetrj.webdep.view.command;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DeslogaUsuarioCommand implements Command {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Invalida sessão e redireciona para login
+		//System.out.println("aqui");
+        //request.getSession().invalidate();
+        response.sendRedirect("index.jsp");
+	}
+ 
+}
