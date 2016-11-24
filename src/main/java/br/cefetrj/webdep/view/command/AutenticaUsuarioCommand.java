eaac5f7825b0b986bde3994b2cbec3dd9f47ec54
Lawrence Fernandes <lawrence.stfs@gmail.com>
2016-11-24 14:13:50 -0200

    Update AutenticaUsuarioCommand.java

--- src/main/java/br/cefetrj/webdep/view/command/AutenticaUsuarioCommand.java
+++ src/main/java/br/cefetrj/webdep/view/command/AutenticaUsuarioCommand.java
@@ -6,9 +6,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.security.MessageDigest;
+
+import br.cefetrj.webdep.model.entity.Usuario;
+import br.cefetrj.webdep.services.UsuarioServices;
+
 /**
+ * Classe responsavel por checar o login e senha do usuario 
+ * no momento de acesso ao sistema.
+ * 
  * @author Lawrence Fernandes
- * @version 0.1
  * @since   12-11-2016 
  */
 
@@ -18,25 +25,24 @@ public class AutenticaUsuarioCommand implements Command {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		boolean autenticado = false;
 		String msg ="Login ou senha incorretos.<br/>";
+		//Login e senha inseridos no index.jsp
 		String loginUsuario = request.getParameter("login");
 		String senhaUsuario = request.getParameter("senha");
-		String senhaCriptografada = this.sha512(senhaUsuario);
-		System.out.println(senhaCriptografada);
-		
+		//Senha inserida criptografada
+		String senhaCriptografada = AutenticaUsuarioCommand.sha512(senhaUsuario);
+		//Login e senha existentes no banco de dados
 		Usuario login = null;
 		String senha = null;
+		
 		try {
 			login = UsuarioServices.validarLogin(loginUsuario);
 			senha = login != null? login.getSenha(): null;
-			System.out.println(senha);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 		if(login != null && senha != null) {
-			//O banco esta salvando a senha "cortada", mesmo apos atualizar o tamanho dela em Usuario
-			//if(senha.equals(senhaCriptografada)) autenticado = true;
-			if(senha.equals(senhaUsuario)) autenticado = true;
+			if(senha.equals(senhaCriptografada)) autenticado = true;
 			else autenticado = false;
 		} else autenticado = false;
 
@@ -48,7 +54,7 @@ public class AutenticaUsuarioCommand implements Command {
 		}	
 	}
 	
-	private String sha512(String passwordToHash) {
+	public static String sha512(String passwordToHash) {
 		try {
 	        MessageDigest digest = MessageDigest.getInstance("SHA-512");
 	        byte[] hash = digest.digest(passwordToHash.getBytes("UTF-8"));
