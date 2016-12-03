f6642c4660f9977fc5a8edaa8fcbf8303928f73b
luanvilarim <luanvilarim@gmail.com>
2016-12-03 10:58:19 -0200

    Adição de Imports

--- src/main/java/br/cefetrj/webdep/model/entity/Permissao.java
+++ src/main/java/br/cefetrj/webdep/model/entity/Permissao.java
@@ -2,10 +2,10 @@ package br.cefetrj.webdep.model.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 /**
--- src/main/java/br/cefetrj/webdep/model/entity/Sistema.java
+++ src/main/java/br/cefetrj/webdep/model/entity/Sistema.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
