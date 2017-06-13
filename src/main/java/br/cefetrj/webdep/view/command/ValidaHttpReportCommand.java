94c70e839d7a78c2c45b006c334bc0f17ea38626
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-06-13 11:49:27 -0300

    Validação dos campos da página HTTPreport.jsp

--- src/main/java/br/cefetrj/webdep/view/command/ValidaHttpReportCommand.java
+++ src/main/java/br/cefetrj/webdep/view/command/ValidaHttpReportCommand.java
@@ -1,6 +1,7 @@
 package br.cefetrj.webdep.view.command;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -8,7 +9,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.cefetrj.webdep.model.entity.PadraoURL;
+import br.cefetrj.webdep.model.entity.Sistema;
 import br.cefetrj.webdep.model.entity.Versao;
+import br.cefetrj.webdep.services.PadraoURLServices;
+import br.cefetrj.webdep.services.SistemaServices;
 import br.cefetrj.webdep.services.VersionServices;
 
 
@@ -19,43 +24,106 @@ public class ValidaHttpReportCommand implements Command {
 		boolean versionValidate = true;
 		boolean errorValidate = true;
 		boolean okValidate = true;
-		String[] versionList;
-		String[] errorList;
-		String[] okList;
+		boolean systemValidate = true;
+		boolean patternValidate = true;
+		String urlPattern = request.getParameter("padraoURL");
+		String[] versionList = request.getParameterValues("versionList");
+		String[] errorList = request.getParameterValues("codigosHttpErro");
+		String[] okList = request.getParameterValues("codigosHttpOk");
 		
-		if(request.getParameterValues("versionList") != null){
-			versionList = request.getParameterValues("versionlist");
-			List<Versao> v = null;
-			for(int i = 0; i < versionList.length; i++){
-				v.add(VersionServices.findByName(versionList[i]));
+		//Valida sistema
+		Sistema sistema = null;
+		if(request.getSession().getAttribute("idsistema") != null){
+			Long idSistema = (Long)request.getSession().getAttribute("idsistema");
+			sistema = SistemaServices.obterPorId(idSistema);
+			if(sistema == null){
+				systemValidate = false;
+				request.setAttribute("systemValidate", systemValidate);
+			}
+		}
+		
+		//Valida padrão de url
+		PadraoURL padrao = null;
+		if(urlPattern != null && urlPattern.trim().length() > 0){
+			Long padraoIdLong = null; 
+			try{
+				padraoIdLong = Long.parseLong(urlPattern);
+				padrao = PadraoURLServices.obterPorId(padraoIdLong);
+				if(padrao == null){
+					patternValidate = false;
+					request.setAttribute("patternValidate", patternValidate);
+				}
+			}catch(Exception e){
+				patternValidate = false;
+				request.setAttribute("patternValidate", patternValidate);
+			}
+		}else{
+			patternValidate = false;
+			request.setAttribute("patternValidate", patternValidate);
+		}
+		
+		//Valida versões
+		if(versionList != null && versionList.length > 0){
+			List<Versao> versaoLista = new ArrayList<Versao>();
+			try{
+				for(String v : versionList){
+					Versao vInst = VersionServices.obterPorId(Long.parseLong(v));
+					if(vInst != null){
+						versaoLista.add(vInst);
+					}
+					else{
+						versionValidate = false;
+						request.setAttribute("versionValidate", versionValidate);
+					}
+				}
+			} catch(Exception e){
+				versionValidate = false;
+				request.setAttribute("versionValidate", versionValidate);
 			}
 		} else {
 			versionValidate = false;
 			request.setAttribute("versionValidate", versionValidate);
 		}
 		
-		if(request.getParameterValues("errorList") != null){
-			errorList = request.getParameterValues("errorList");
-		} else {
-			errorValidate = false;
-			request.setAttribute("errorValidate", errorValidate);
-		}
-		
-		
-		if(request.getParameterValues("okList") != null){
-			okList = request.getParameterValues("okList");
-		} else {
+		//Valida códigos http ok
+		List<Integer> codigosOk = new ArrayList<Integer>();
+		if(okList != null && okList.length > 0){
+			try{
+				for (String c: okList) {
+					codigosOk.add(Integer.parseInt(c));
+				}
+			}catch(Exception e){
+				okValidate = false;
+				request.setAttribute("okValidate", okValidate);
+			}
+		}else{
 			okValidate = false;
 			request.setAttribute("okValidate", okValidate);
 		}
 		
+		//Valida códigos http erro
+		List<Integer> codigosErro = new ArrayList<Integer>();
+		if(errorList != null && errorList.length > 0){
+			try{
+				for (String c: errorList) {
+					codigosErro.add(Integer.parseInt(c));
+				}
+			}catch(Exception e){
+				errorValidate = false;
+				request.setAttribute("errorValidate", errorValidate);
+			}
+		}else{
+			errorValidate = false;
+			request.setAttribute("errorValidate", errorValidate);
+		}
+		
+		//Retornar para a jsp caso algum campo seja inválido
 		if((versionValidate == false) || (errorValidate == false) || (okValidate = false)){
 			request.getRequestDispatcher("HTTPreport.jsp").forward(request, response);
 			return;
-		} else {
-			//L�gica para a busca dos filtros...
 		}
 		
+		//Chamar o service para retornar os dados da query
 	}
 
 }
