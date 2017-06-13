7d557b57b1b70500b86e822e0ff7d7e55d300db7
diogosmendonca <diogosmendonca@gmail.com>
2017-06-12 21:16:24 -0300

    Inclusão da infraestrutura para geração de gráficos em R e um exemplo.

--- src/main/java/br/cefetrj/webdep/view/tag/BoxPlotRChartTag.java
+++ src/main/java/br/cefetrj/webdep/view/tag/BoxPlotRChartTag.java
@@ -1,10 +1,10 @@
 package br.cefetrj.webdep.view.tag;
 
-import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.tagext.SimpleTagSupport;
+import org.apache.log4j.Logger;
+import org.rosuda.REngine.REngineException;
 
 /**
  * Gráfico de boxplot
@@ -12,15 +12,40 @@ import javax.servlet.jsp.tagext.SimpleTagSupport;
  * @author diogo
  *
  */
-public class BoxPlotRChartTag extends SimpleTagSupport {
-	
-	private List<List<Double>> dados;
-	
-	private List<String> labels;
-	
-	@Override
-	public void doTag() throws JspException, IOException {
-		
-	}
+public class BoxPlotRChartTag extends RChartCommand {
 
+	@Override
+	public void plot(Object dados) {
+		int[] dados2 = {15, 32, 87, 100, 112, 34, 65, 29}; 
+		try {
+			List<Map<String, Long>> list = (List<Map<String, Long>>) dados;
+			Map<String, Long> contagemAcessosUrlsSemFalha = list.get(0);
+			Map<String, Long> contagemAcessosUrlsComFalha = list.get(1);
+			
+			double[] acessosSemFalha = new double[contagemAcessosUrlsSemFalha.values().size()];
+			int i = 0;
+			for (Long value: contagemAcessosUrlsSemFalha.values()) {
+				acessosSemFalha[i] = value;
+				i++;
+			}
+			c.assign("acessosSemFalha", acessosSemFalha);
+			
+			double[] acessosComFalha = new double[contagemAcessosUrlsComFalha.values().size()];
+			i = 0;
+			for (Long value: contagemAcessosUrlsComFalha.values()) {
+				acessosComFalha[i] = value;
+				i++;
+			}
+			c.assign("acessosComFalha", acessosComFalha);
+			
+			c.eval("boxplot(acessosSemFalha, acessosComFalha, "
+					+ " names = c('URLs sem Falha', 'URL com Falha'), "
+					+ " ylab = 'número de acessos')"); 
+			
+		} catch (REngineException e) {
+			Logger lg = Logger.getLogger(BoxPlotRChartTag.class);
+			lg.error("Não foi possível gerar o gráfico Box Plot", e);
+		}
+	}
+	
 }
