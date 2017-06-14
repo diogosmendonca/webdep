72a66eff3fa334c02a999b17b5993c454348584e
Aluno <Aluno>
2017-06-14 20:39:40 -0300

    Gráfico de relatório de falhas de acesso ok.

--- src/main/java/br/cefetrj/webdep/view/tag/ScatterPlotHttpReportRChartTag.java
+++ src/main/java/br/cefetrj/webdep/view/tag/ScatterPlotHttpReportRChartTag.java
@@ -1,45 +1,50 @@
 package br.cefetrj.webdep.view.tag;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.rosuda.REngine.REngineException;
 
+/**
+ * Gráfico de boxplot
+ * 
+ * @author caiopotter
+ *
+ */
 public class ScatterPlotHttpReportRChartTag extends RChartCommand {
-	
+
 	@Override
 	public void plot(Object dados) {
 		try {
 			List<Map<String, Long>> list = (List<Map<String, Long>>) dados;
-			Map<String, Long> contagemAcessosUrls = list.get(0);
+			Map<String, Long> contagemAcessosUrlsSemFalha = list.get(0);
 			Map<String, Long> contagemAcessosUrlsComFalha = list.get(1);
-			
-			double[] acessosTotais = new double[contagemAcessosUrls.values().size()];
-	
-			int i = 0;
-			for (Long value: contagemAcessosUrls.values()) {
-				acessosTotais[i] = value;
-				i++;
-			}
-			c.assign("acessosTotais", acessosTotais);
-			
-			
+			List<String> acessoList = new ArrayList<String>(contagemAcessosUrlsComFalha.keySet());
+			String acessoConcat = "";
+			int i;
 			double[] acessosComFalha = new double[contagemAcessosUrlsComFalha.values().size()];
 			i = 0;
 			for (Long value: contagemAcessosUrlsComFalha.values()) {
 				acessosComFalha[i] = value;
 				i++;
 			}
+			
+			for (int j=0; j < acessoList.size(); j++){
+				acessoConcat += acessoList.get(j) + ", ";
+			}
+			
 			c.assign("acessosComFalha", acessosComFalha);
-		
-			c.eval("plot(acessosTotais, acessosComFalha, "
-					+ " xlab = 'Número de Acessos', "
-					+ " ylab = 'Número de Falhas')"); 
+			c.assign("acessoConcat", acessoConcat);
+			c.eval("acessoList = as.list(strsplit(acessoConcat, ',')[[1]])");
+			c.eval("plot(acessosSemFalha, yaxt = 'n', xlab ='numero de acessos', ylab = c('URLs com Falha'))");
+			c.eval("axis(2, at=1:acessosSemFalha, labels=acessoList[1:acessosSemFalha])");
 			
 		} catch (REngineException e) {
-			Logger lg = Logger.getLogger(BoxPlotRChartTag.class);
-			lg.error("NÃ£o foi possÃ­vel gerar o grÃ¡fico Scatter Plot", e);
+			Logger lg = Logger.getLogger(ScatterPlotHttpReportRChartTag.class);
+			lg.error("Não foi possível gerar o gráfico Plot", e);
 		}
 	}
+	
 }
